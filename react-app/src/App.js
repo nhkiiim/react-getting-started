261642639166605f28d7ce719981c79e49a6bb22
nhkiiim <nhkiiim@naver.com>
2023-02-28 22:29:34 +0900

    Feat: 컴포넌트 만들기

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,52 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
+
+class Subject extends Component {
+  render() { // js class 안에서는 return 생략 가능
+    // return은 하나의 최상위 태그로 이루어져야함
+    return (
+      <header> 
+        <h1>WEB</h1>
+        world wide web!
+      </header>
+    );
+  }
+}
+
+class TOC extends Component { // table of content
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.html">CSS</a></li>
+          <li><a href="3.html">JavaScript</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>
+        <h2>HTML</h2>
+        HTML is HyperText Markup Lanuage.
+      </article>
+    );
+  }
+}
+
+// js 문법 아님! -> jsx (create-react-app이 변환 해줌)
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello React
+        <Subject></Subject>
+        <TOC></TOC>
+        <Content></Content>
       </div>
     );
   }
