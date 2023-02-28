e5722d48dd1a573b4ca4f715b1082e7fdcbc481a
nhkiiim <nhkiiim@naver.com>
2023-02-28 22:38:07 +0900

    Add: props 사용

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,8 +7,8 @@ class Subject extends Component {
     // return은 하나의 최상위 태그로 이루어져야함
     return (
       <header> 
-        <h1>WEB</h1>
-        world wide web!
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
       </header>
     );
   }
@@ -32,8 +32,8 @@ class Content extends Component {
   render() {
     return (
       <article>
-        <h2>HTML</h2>
-        HTML is HyperText Markup Lanuage.
+        <h2>{this.props.title}</h2>
+        {this.props.sub}
       </article>
     );
   }
@@ -44,9 +44,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Subject></Subject>
+        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject title="React" sub="For UI"></Subject>
         <TOC></TOC>
-        <Content></Content>
+        <Content title="HTML" sub="HTML is HyperText Markup Lanuage."></Content>
       </div>
     );
   }
